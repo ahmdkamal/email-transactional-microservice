920dc4959bbb5d068dd95c2fdc3344986eccaa56
kamal <aka.3awd@gmail.com>
2021-06-08 21:59:45 +0200

    add Mailer Server setter to send message object

--- src/app/Services/SendMessage.php
+++ src/app/Services/SendMessage.php
@@ -3,22 +3,25 @@
 namespace App\Services;
 
 use App\Entities\Mail;
+use App\Services\Interfaces\InterfaceSendMessage;
 use App\Services\Interfaces\Mailer;
 
-class MailMessage
+class SendMessage implements InterfaceSendMessage
 {
     protected Mail $mail;
 
     protected Mailer $mailer;
 
-    public function __construct(Mail $mail)
+    public function setMail(Mail $mail): SendMessage
     {
         $this->mail = $mail;
+        return $this;
     }
 
-    public function setMailServer(Mailer $mailer): void
+    public function setMailServer(Mailer $mailer): SendMessage
     {
         $this->mailer = $mailer;
+        return $this;
     }
 
     public function send(): bool
